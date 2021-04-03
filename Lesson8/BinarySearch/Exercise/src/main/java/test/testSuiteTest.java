13a028d16ee4e0f38483dce1155328023d038320
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2021-04-03 20:00:21 +0200

    d

---
+++ Lesson8/BinarySearch/Exercise/src/main/java/test/testSuiteTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({test.IterativeBinarySearchTest.class, test.RecursiveBinarySearchTest.class})
+public class testSuiteTest {
+}
